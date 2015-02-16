070ffdce9a5efc7dfa360d328fd4c4e91f0eccb6
cristianomatos <cristianobmatos@gmail.com>
2015-02-16 19:18:42 -0200

    [Base] Volume panel timeout
    - This feature keeps Google behavior on safe warning and zen panel expanded mode. All the others media will respect the custom timeouts.
    
    This feature is based in kk feature, from here: https://github.com/cristianomatos/android_frameworks_base/commit/0958e539f568352f0d44a29481275329d7c3a9dd. Thanks @UtkarshGupta for the idea
    
    Change-Id: I6197492bdf957cc9fa4258e1950c6cc875a4f431
    Signed-off-by: Cristiano Matos <cristianomatos@gmail.com>
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -110,6 +110,7 @@ public class VolumePanel extends Handler implements DemoMode {
     private static final int TIMEOUT_DELAY_COLLAPSED = 4500;
     private static final int TIMEOUT_DELAY_SAFETY_WARNING = 5000;
     private static final int TIMEOUT_DELAY_EXPANDED = 10000;
+    private static final int TIMEOUT_DELAY_VOL_PANEL = 3000;
     private static final int ANIMATION_DURATION = 300; // ms
 
     private static final int MSG_VOLUME_CHANGED = 0;
@@ -156,6 +157,7 @@ public class VolumePanel extends Handler implements DemoMode {
     private boolean mExtendedPanelExpanded = false;
     private boolean mVolumeLinkNotification;
     private int mTimeoutDelay = TIMEOUT_DELAY;
+    private int mVolPanelTimeoutDelay = TIMEOUT_DELAY_VOL_PANEL;
     private float mDisabledAlpha;
     private int mLastRingerMode = AudioManager.RINGER_MODE_NORMAL;
     private int mLastRingerProgress = 0;
@@ -290,6 +292,8 @@ public class VolumePanel extends Handler implements DemoMode {
         public void onChange(boolean selfChange) {
             mVolumeLinkNotification = Settings.Secure.getInt(mContext.getContentResolver(),
                     Settings.Secure.VOLUME_LINK_NOTIFICATION, 1) == 1;
+            mVolPanelTimeoutDelay = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.VOLUME_PANEL_TIMEOUT, TIMEOUT_DELAY_VOL_PANEL);
         }
     };
 
@@ -445,7 +449,7 @@ public class VolumePanel extends Handler implements DemoMode {
         Interaction.register(mView, new Interaction.Callback() {
             @Override
             public void onInteraction() {
-                resetTimeout();
+                resetVolPanelTimeout();
             }
         });
 
@@ -463,8 +467,14 @@ public class VolumePanel extends Handler implements DemoMode {
         mVolumeLinkNotification = Settings.Secure.getInt(mContext.getContentResolver(),
                 Settings.Secure.VOLUME_LINK_NOTIFICATION, 1) == 1;
 
+        mVolPanelTimeoutDelay = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.VOLUME_PANEL_TIMEOUT, TIMEOUT_DELAY_VOL_PANEL);
+
         context.getContentResolver().registerContentObserver(
                 Settings.Secure.getUriFor(Settings.Secure.VOLUME_LINK_NOTIFICATION), false,
+                mSettingsObserver);
+        context.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.VOLUME_PANEL_TIMEOUT), false,
                 mSettingsObserver);
 
         if (mZenController != null && !useMasterVolume) {
@@ -531,6 +541,7 @@ public class VolumePanel extends Handler implements DemoMode {
         pw.print("  mZenPanelExpanded="); pw.println(mZenPanelExpanded);
         pw.print("  mNotificationEffectsSuppressor="); pw.println(mNotificationEffectsSuppressor);
         pw.print("  mTimeoutDelay="); pw.println(mTimeoutDelay);
+        pw.print("  mVolPanelTimeoutDelay="); pw.println(mVolPanelTimeoutDelay);
         pw.print("  mDisabledAlpha="); pw.println(mDisabledAlpha);
         pw.print("  mLastRingerMode="); pw.println(mLastRingerMode);
         pw.print("  mLastRingerProgress="); pw.println(mLastRingerProgress);
@@ -726,7 +737,7 @@ public class VolumePanel extends Handler implements DemoMode {
                     sc.icon.setOnClickListener(new OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            resetTimeout();
+                            resetVolPanelTimeout();
                             toggleRinger(sc);
                         }
                     });
@@ -744,7 +755,7 @@ public class VolumePanel extends Handler implements DemoMode {
                 @Override
                 public void onClick(View v) {
                     expandVolumePanel();
-                    resetTimeout();
+                    resetVolPanelTimeout();
                 }
             });
             sc.seekbarView.setMax(getStreamMaxVolume(streamType));
@@ -777,7 +788,6 @@ public class VolumePanel extends Handler implements DemoMode {
             active.group.setVisibility(View.VISIBLE);
             updateSlider(active, true /*forceReloadIcon*/);
         }
-        updateTimeoutDelay();
         updateZenPanelVisible();
         hideVolumePanel();
     }
@@ -998,13 +1008,14 @@ public class VolumePanel extends Handler implements DemoMode {
                 final View.OnTouchListener showHintOnTouch = new View.OnTouchListener() {
                     @Override
                     public boolean onTouch(View v, MotionEvent event) {
-                        resetTimeout();
+                        resetVolPanelTimeout();
                         if (mAudioManager.getRingerModeInternal() ==
                                 AudioManager.RINGER_MODE_VIBRATE) {
                             showRingerHint();
                         } else {
                             showSilentHint();
                         }
+                        showSilentHint();
                         return false;
                     }
                 };
@@ -1053,7 +1064,6 @@ public class VolumePanel extends Handler implements DemoMode {
         mTimeoutDelay = mDemoIcon != 0 ? TIMEOUT_DELAY_EXPANDED
                 : sSafetyWarning != null ? TIMEOUT_DELAY_SAFETY_WARNING
                 : mZenPanelExpanded ? TIMEOUT_DELAY_EXPANDED
-                : isZenPanelVisible() ? TIMEOUT_DELAY_COLLAPSED
                 : mActiveStreamType == AudioManager.STREAM_MUSIC ? TIMEOUT_DELAY_SHORT
                 : TIMEOUT_DELAY;
     }
@@ -1215,7 +1225,7 @@ public class VolumePanel extends Handler implements DemoMode {
 
         removeMessages(MSG_FREE_RESOURCES);
         sendMessageDelayed(obtainMessage(MSG_FREE_RESOURCES), FREE_DELAY);
-        resetTimeout();
+        resetVolPanelTimeout();
     }
 
     protected void onMuteChanged(int streamType, int flags) {
@@ -1480,7 +1490,7 @@ public class VolumePanel extends Handler implements DemoMode {
 
         removeMessages(MSG_FREE_RESOURCES);
         sendMessageDelayed(obtainMessage(MSG_FREE_RESOURCES), FREE_DELAY);
-        resetTimeout();
+        resetVolPanelTimeout();
     }
 
     protected void onRemoteVolumeUpdateIfShown() {
@@ -1726,6 +1736,18 @@ public class VolumePanel extends Handler implements DemoMode {
         }
     }
 
+    private void resetVolPanelTimeout() {
+        final boolean touchExploration = mAccessibilityManager.isTouchExplorationEnabled();
+        if (LOGD) Log.d(mTag, "resetVolPanelTimeout at " + System.currentTimeMillis()
+                + " delay=" + mVolPanelTimeoutDelay + " touchExploration=" + touchExploration);
+        if (sSafetyWarning == null || !touchExploration) {
+            removeMessages(MSG_TIMEOUT);
+            sendEmptyMessageDelayed(MSG_TIMEOUT, mVolPanelTimeoutDelay);
+            removeMessages(MSG_USER_ACTIVITY);
+            sendEmptyMessage(MSG_USER_ACTIVITY);
+        }
+    }
+
     private void forceTimeout(long delay) {
         if (LOGD) Log.d(mTag, "forceTimeout delay=" + delay + " callers=" + Debug.getCallers(3));
         removeMessages(MSG_TIMEOUT);
@@ -1766,7 +1788,7 @@ public class VolumePanel extends Handler implements DemoMode {
                 setStreamVolume(sc, progress,
                         AudioManager.FLAG_SHOW_UI | AudioManager.FLAG_VIBRATE);
             }
-            resetTimeout();
+            resetVolPanelTimeout();
         }
 
         @Override
