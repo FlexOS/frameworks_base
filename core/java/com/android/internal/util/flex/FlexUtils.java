79db01910d5bd42cb862b35d948703163e60f969
martincz <martincz@mokeedev.com>
2015-03-24 19:56:00 +0100

    Show carrier label / custom & change color [1/2]
    AICPfy:
    No DuUtils
    PS2: fix build
    PS3: lower font size to 14dp
    PS4: Use Statusbar clock style instead
    
    Change-Id: Ida3321c9176e4e786fad96766dab056d8614369d
    
    Conflicts:
    	packages/SystemUI/res/layout/status_bar.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- core/java/com/android/internal/util/flex/FlexUtils.java
+++ core/java/com/android/internal/util/flex/FlexUtils.java
@@ -17,6 +17,9 @@
 package com.android.internal.util.flex;
 
 import android.content.Context;
+import android.content.res.Resources;
+
+import java.util.Locale;
 
 public class FlexUtils {
 
@@ -24,4 +27,10 @@ public class FlexUtils {
         return context.getResources().getBoolean(
                 com.android.internal.R.bool.config_showNavigationBar);
     }
+
+    public static boolean isChineseLanguage() {
+       return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(
+               Locale.CHINESE.getLanguage());
+    }
+
 }
