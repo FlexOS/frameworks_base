e6981f2b7f5da605cb5139788e701d77d8a1ca14
eyosen <abittin@gmail.com>
2015-03-24 16:00:06 +0100

    screenrecord: fix key combo trigger
    Change-Id: I9347530be4d2835ef57df1813b43628319522671

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -5667,10 +5667,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             mVolumeUpKeyTriggered = true;
                             mVolumeUpKeyTime = event.getDownTime();
                             mVolumeUpKeyConsumedByScreenshotChord = false;
+                            mVolumeUpKeyConsumedByScreenrecordChord = false;
                             mScreenshotChordVolumeUpKeyTriggered = true;
                             cancelPendingPowerKeyAction();
                             cancelPendingScreenshotChordAction();
                             interceptScreenshotLog();
+                            interceptScreenrecordChord();
                         }
                     } else {
                         mScreenshotChordVolumeUpKeyTriggered = false;
