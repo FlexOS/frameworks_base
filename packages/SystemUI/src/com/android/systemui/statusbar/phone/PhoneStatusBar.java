5f872c71bc6de112e95fd2dc6d6fb9dd8c6887f5
Aniruddh Chandratre <chandratre.aniruddh@gmail.com>
2015-04-12 06:48:12 -0400

    Revert "Detect status bar carrier state when theme change."
    This reverts commit d7fb5ad8d020e2ce45261c3924e5efb2322cd8d0.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3911,8 +3911,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
      * meantime, just update the things that we know change.
      */
     void updateResources(Configuration newConfig) {
-        ContentResolver resolver = mContext.getContentResolver();
-
         // detect theme change.
         ThemeConfig newTheme = newConfig != null ? newConfig.themeConfig : null;
         final boolean updateStatusBar = shouldUpdateStatusbar(mCurrentTheme, newTheme);
@@ -3920,12 +3918,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (updateStatusBar) {
             mContext.recreateTheme();
             recreateStatusBar();
-
-            // detect status bar carrier state when theme change.
-            mShowStatusBarCarrier = Settings.System.getInt(
-                    resolver, Settings.System.STATUS_BAR_CARRIER, 0) == 1;
-            showStatusBarCarrierLabel(mShowStatusBarCarrier);
-
         } else {
             loadDimens();
         }
