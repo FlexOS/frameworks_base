83122edeb120d711b99b8a7f9cea47ab99ea9ce6
Flex-OS <flexos.2015@gmail.com>
2015-04-25 03:28:16 -0400

    Revert "Fix make updateapi for custom density setting"
    This reverts commit 2ac8581869f3f86618dfa68254447ee1e8df7e82.

--- core/java/android/util/DisplayMetrics.java
+++ core/java/android/util/DisplayMetrics.java
@@ -124,10 +124,6 @@ public class DisplayMetrics {
     @Deprecated
     public static int DENSITY_DEVICE;
 
-    /**
-     * Allow custom density setting
-     * @hide
-     */
     public static int DENSITY_CURRENT;
 
     static {
@@ -225,10 +221,6 @@ public class DisplayMetrics {
      */
     public float noncompatYdpi;
 
-    /**
-     * Allow custom density setting
-     * @hide
-     */
     public void updateDensity() {
         density = DENSITY_CURRENT / (float) DENSITY_DEFAULT;
         densityDpi = DENSITY_CURRENT;
@@ -333,11 +325,7 @@ public class DisplayMetrics {
             ", height=" + heightPixels + ", scaledDensity=" + scaledDensity +
             ", xdpi=" + xdpi + ", ydpi=" + ydpi + "}";
     }
-    
-    /**
-     * Allow custom density setting
-     * @hide
-     */
+
     public static int getDeviceDensity() {
         return DENSITY_CURRENT;
     }
