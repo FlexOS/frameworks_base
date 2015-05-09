2ec2f0ae08f8a72f2e68d73f92ec8772708f436d
Flex-OS <flexos.2015@gmail.com>
2015-05-09 06:41:35 -0400

    Revert "SystemUI: update notification volume slider state when toggling modes"
    This reverts commit 9826c0bf13d933608c8cf8d666571a88b0ba3506.

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -748,9 +748,6 @@ public class VolumePanel extends Handler implements DemoMode {
                     public void onClick(View v) {
                         resetVolPanelTimeout();
                         toggleRinger(sc);
-                        if (!mVolumeLinkNotification) {
-                            updateStates();
-                        }
                     }
                 });
                 sc.iconSuppressedRes = com.android.systemui.R.drawable.ic_ringer_mute;
