965442f13b2fbfc4e009370195f14c009de09a70
Flex-OS <flexos.2015@gmail.com>
2015-05-09 06:26:48 -0400

    Revert "Navigation bar button color (1/2)"
    This reverts commit 6cad08e3bd7b006e3d57abe026f0cacd7eb9085a.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2742,12 +2742,6 @@ public final class Settings {
         public static final String NAVIGATION_BAR_WIDTH = "navigation_bar_width";
 
         /**
-         * Navigation bar button color
-         * @hide
-         */
-        public static final String NAVIGATION_BAR_TINT = "navigation_bar_tint";
-
-        /**
          * Enable HW keys
          * @hide
          */
