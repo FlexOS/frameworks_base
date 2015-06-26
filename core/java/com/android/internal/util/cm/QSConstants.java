k2wl/android_frameworks_base

NavBar tile [1/3]
Change-Id: I90727432c1ea806170ef9b13d5237eabe69dd14c

Conflicts:
	core/java/com/android/internal/util/cm/QSConstants.java
	packages/SystemUI/res/values/strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

Conflicts:
	packages/SystemUI/res/values/cmr_strings.xml

core/java/com/android/internal/util/cm/QSConstants.java
@@ -54,6 +54,7 @@ public class QSConstants {
     public static final String TILE_EXPANDED_DESKTOP = "expanded_desktop";
     public static final String TILE_REBOOT = "reboot";
     public static final String TILE_HEADS_UP = "toggleHeadsUp";
+    public static final String TILE_NAVBAR = "toggleNavBar";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -98,5 +99,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_EXPANDED_DESKTOP);
         TILES_AVAILABLE.add(TILE_REBOOT);
         TILES_AVAILABLE.add(TILE_HEADS_UP);
+        TILES_AVAILABLE.add(TILE_NAVBAR);
     }
 }

FlexOS/frameworks_base

	packages/SystemUI/res/values/cm_strings.xml
	packages/SystemUI/res/values/flex_strings.xml

core/java/com/android/internal/util/cm/QSConstants.java
@@ -50,6 +50,7 @@ public class QSConstants {
     public static final String TILE_HEADS_UP = "heads_up";
     public static final String TILE_AMBIENT_DISPLAY = "ambient_display";
     public static final String TILE_SYNC = "sync";
+    public static final String TILE_NAVBAR = "toggleNavBar";
 
     public static final String DYNAMIC_TILE_NEXT_ALARM = "next_alarm";
     public static final String DYNAMIC_TILE_IME_SELECTOR = "ime_selector";
@@ -90,6 +91,7 @@ public class QSConstants {
         STATIC_TILES_AVAILABLE.add(TILE_HEADS_UP);
         STATIC_TILES_AVAILABLE.add(TILE_AMBIENT_DISPLAY);
         STATIC_TILES_AVAILABLE.add(TILE_SYNC);
+        STATIC_TILES_AVAILABLE.add(TILE_NAVBAR);
 
         DYNAMIC_TILES_AVAILABLE.add(DYNAMIC_TILE_NEXT_ALARM);
         DYNAMIC_TILES_AVAILABLE.add(DYNAMIC_TILE_IME_SELECTOR);